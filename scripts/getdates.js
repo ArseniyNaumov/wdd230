02c81755f523024a2292a9e09a50b3f36314c371
arseniy.v.naumov <arseniy.v.naumov@gmail.com>
2024-01-19 13:08:20 -0600

    Creating a clone 231

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// Getting the current year
+let currentYear = new Date().getFullYear();
+// Update the content of the 'current-year' span with the current year
+document.getElementById('current-year').textContent = currentYear;
+
+// Getting the current date
+let currentDate = new Date();
+// Update the content of the 'last modifyed' span with the current date
+document.getElementById('lastModified').textContent = currentDate;
