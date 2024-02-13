831db6ade10ae4a2611e177b5b72aeeaff5ba607
arseniy.v.naumov <nau13002@byui.edu>
2024-02-13 14:52:50 -0600

    fixed color changings

--- chamber/scripts/functionality.js
+++ chamber/scripts/functionality.js
@@ -48,16 +48,20 @@ function showSlides(n) {
 
 // Dark Mode
 const modeSwitch = document.getElementById("modeSwitch");
+const h2Element = document.querySelector("h2");
 
 modeSwitch.addEventListener("change", function () {
     const isChecked = this.checked;
     if (isChecked) {
         document.body.style.backgroundColor = "#0e0906";
-        document.h2.style.color = "#ff9b28";
+        h2Element.style.color = "#ff9b28";
+        h2Element.style.textShadow = '-1px 1px 0 #FAF8EA';
         alert("Dark mode is on");
     } else {
         document.body.style.backgroundColor = "#FAF8EA";
-        document.h2.style.color = "#CE6E00";
+        h2Element.style.color = "#CE6E00";
+        h2Element.style.textShadow = '-2px 2px 0 #25160B';
+
         alert("Dark mode is off");
     }
 });
