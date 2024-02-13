87de0db97c6d3ce4ef29634a0bd3abc27c661096
arseniy.v.naumov <nau13002@byui.edu>
2024-02-13 11:29:35 -0600

    fixed

---
+++ chamber/scripts/functionality.js
@@ -0,0 +1,44 @@
+// HAMBURGER MENU CODE
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+console.log("this file is linked");
+
+
+
+
+
+
+// SLIDESHOW
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
--- chamber/sctipts/functionality.js
+++
@@ -1,8 +0,0 @@
-// HAMBURGER MENU CODE
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('nav');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
