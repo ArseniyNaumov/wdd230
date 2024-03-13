6fb5cb52b8dffd6b86fe54f06af28d1ceb7924fe
Arseniy Naumov <arseniy.v.naumov@gmail.com>
2024-03-13 16:53:04 -0500

    Weather API works

--- scripts/mainScripts.js
+++ scripts/mainScripts.js
@@ -32,3 +32,35 @@ if (numVisits !== 0) {
 }
 numVisits++;//this is the line I ommitted and ChatGPT helped me realize I needed this to function properly
 localStorage.setItem("numVisits-ls", numVisits);
+
+//WEATHER API
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.76&lon=6.65&units=imperial&appid=b762dd22fc236e01b76f3061c1f87dd8`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon.substring(0, 3)}.png`;
+    const desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
