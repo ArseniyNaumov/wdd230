1a0fa8e5e5ca60a2c56bfaf158b2c73995976b93
Arseniy Naumov <arseniy.v.naumov@gmail.com>
2024-03-17 17:03:07 -0500

    weather code bug fixed

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,31 +1,28 @@
-//WEATHER API
+//WEATHER CODE using API from https://api.openweathermap.org
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+const weatherIcon = document.querySelector('#weather-icon');// this line was troubleshooted with ChatGPT
 const captionDesc = document.querySelector('figcaption');
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.76&lon=6.65&units=imperial&appid=b762dd22fc236e01b76f3061c1f87dd8`;
-
 async function apiFetch() {
+    const apiKey = 'b762dd22fc236e01b76f3061c1f87dd8';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Spring,US&units=imperial&appid=${apiKey}`;
     try {
         const response = await fetch(url);
         if (response.ok) {
-            const data = await response.json();
+            const data = await response.json();// this line was troubleshooted with ChatGPT
             displayResults(data);
         } else {
             throw Error(await response.text());
-
         }
     } catch (error) {
         console.log(error);
     }
 }
-
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon.substring(0, 3)}.png`;
-    const desc = data.weather[0].description;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description;// this line was troubleshooted with ChatGPT
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('alt', desc);// this line was troubleshooted with ChatGPT
     captionDesc.textContent = `${desc}`;
 }
-
 apiFetch();
