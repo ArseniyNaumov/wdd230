cbba835896b72f9a7a3c5d65dea30538de11f57b
Arseniy Naumov <arseniy.v.naumov@gmail.com>
2024-04-08 23:22:25 -0500

    links for adress hooked up to google map

--- FinalProject/scripts/weatherMessage.js
+++ FinalProject/scripts/weatherMessage.js
@@ -16,12 +16,12 @@ window.onload = function () {
 };
 
 const apiKey = 'b762dd22fc236e01b76f3061c1f87dd8';
-const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=20.42&lon=-86.92&exclude=minutely,hourly,daily&appid=${apiKey}&units=imperial`;
+const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=20.42&lon=-86.92&exclude=minutely,hourly,daily&appid=${apiKey}&units=imperial`;
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        const forecast = data.current.temp;
+        const forecast = data.daily.temp.max;
         document.getElementById('weatherMessage').innerHTML = `It can get as hot as ${forecast}&#x2109; today on Cozumel Island! <br>Wear hat, keep hydrated and use sunscreen!`;
         alert('worked');
     })
