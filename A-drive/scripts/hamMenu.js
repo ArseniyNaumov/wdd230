a2dfaa5810bfd12dcd9729f9032eedcae6c34f85
Arseniy V. Naumov <arseniy.v.naumov@gmail.com>
2025-10-21 15:07:06 -0500

    Head done+hamburger menu

---
+++ A-drive/scripts/hamMenu.js
@@ -0,0 +1,7 @@
+// HAMBURGER MENU CODE
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
